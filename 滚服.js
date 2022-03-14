600b0742077efed8c232b09a71b454057a1e37c4
Liam Ma <lma2@atlassian.com>
2022-03-14 22:07:01 +1100

    滚服

---
+++ 滚服.js
@@ -0,0 +1,29 @@
+console.log("用户中心");
+click(1327, 101);
+sleep(2000);
+
+console.log("用户");
+click(282, 608);
+sleep(2000);
+
+console.log("continue");
+click(1029, 823);
+sleep(2000);
+
+console.log("enter confirm");
+click(730, 617);
+sleep(2000);
+click(812, 542);
+sleep(2000);
+click(316, 744);
+sleep(2000);
+click(1201, 473);
+sleep(2000);
+
+console.log("enter");
+click(942, 903);
+sleep(10000);
+
+console.log("确认");
+click(1158, 870);
+sleep(2000);
